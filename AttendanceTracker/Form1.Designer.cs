40523a6312f1d9139b9ab27a4b0dcfca439a5570
Gian <Gian@LAPTOP-EMVPPAV4>
2024-12-10 22:52:26 +0800

    Add project files.

---
+++ AttendanceTracker/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace AttendanceTracker
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox txtEmployeeID;
+        private System.Windows.Forms.TextBox txtEmployeeName;
+        private System.Windows.Forms.ComboBox cbAttendanceStatus;
+        private System.Windows.Forms.Button btnAddAttendance;
+        private System.Windows.Forms.Button btnUpdateAttendance;
+        private System.Windows.Forms.DataGridView dgvAttendance;
+        private System.Windows.Forms.Button btnSaveData;
+        private System.Windows.Forms.Button btnLoadData;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtEmployeeID = new System.Windows.Forms.TextBox();
+            this.txtEmployeeName = new System.Windows.Forms.TextBox();
+            this.cbAttendanceStatus = new System.Windows.Forms.ComboBox();
+            this.btnAddAttendance = new System.Windows.Forms.Button();
+            this.btnUpdateAttendance = new System.Windows.Forms.Button();
+            this.dgvAttendance = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnSaveData = new System.Windows.Forms.Button();
+            this.btnLoadData = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAttendance)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtEmployeeID
+            // 
+            this.txtEmployeeID.Location = new System.Drawing.Point(151, 38);
+            this.txtEmployeeID.Name = "txtEmployeeID";
+            this.txtEmployeeID.Size = new System.Drawing.Size(250, 26);
+            this.txtEmployeeID.TabIndex = 0;
+            // 
+            // txtEmployeeName
+            // 
+            this.txtEmployeeName.Location = new System.Drawing.Point(151, 64);
+            this.txtEmployeeName.Name = "txtEmployeeName";
+            this.txtEmployeeName.Size = new System.Drawing.Size(250, 26);
+            this.txtEmployeeName.TabIndex = 1;
+            // 
+            // cbAttendanceStatus
+            // 
+            this.cbAttendanceStatus.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbAttendanceStatus.FormattingEnabled = true;
+            this.cbAttendanceStatus.Location = new System.Drawing.Point(151, 90);
+            this.cbAttendanceStatus.Name = "cbAttendanceStatus";
+            this.cbAttendanceStatus.Size = new System.Drawing.Size(250, 26);
+            this.cbAttendanceStatus.TabIndex = 2;
+            // 
+            // btnAddAttendance
+            // 
+            this.btnAddAttendance.Location = new System.Drawing.Point(151, 134);
+            this.btnAddAttendance.Name = "btnAddAttendance";
+            this.btnAddAttendance.Size = new System.Drawing.Size(100, 30);
+            this.btnAddAttendance.TabIndex = 3;
+            this.btnAddAttendance.Text = "Add";
+            this.btnAddAttendance.UseVisualStyleBackColor = true;
+            this.btnAddAttendance.Click += new System.EventHandler(this.btnAddAttendance_Click);
+            // 
+            // btnUpdateAttendance
+            // 
+            this.btnUpdateAttendance.Location = new System.Drawing.Point(301, 134);
+            this.btnUpdateAttendance.Name = "btnUpdateAttendance";
+            this.btnUpdateAttendance.Size = new System.Drawing.Size(100, 30);
+            this.btnUpdateAttendance.TabIndex = 4;
+            this.btnUpdateAttendance.Text = "Update";
+            this.btnUpdateAttendance.UseVisualStyleBackColor = true;
+            this.btnUpdateAttendance.Click += new System.EventHandler(this.btnUpdateAttendance_Click);
+            // 
+            // dgvAttendance
+            // 
+            this.dgvAttendance.BackgroundColor = System.Drawing.Color.LightCyan;
+            this.dgvAttendance.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAttendance.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3});
+            this.dgvAttendance.GridColor = System.Drawing.Color.DimGray;
+            this.dgvAttendance.Location = new System.Drawing.Point(12, 178);
+            this.dgvAttendance.Name = "dgvAttendance";
+            this.dgvAttendance.Size = new System.Drawing.Size(616, 250);
+            this.dgvAttendance.TabIndex = 5;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Employee ID";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 170;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Employee Name";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.Width = 200;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "Attendance Status";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.Width = 200;
+            // 
+            // btnSaveData
+            // 
+            this.btnSaveData.Location = new System.Drawing.Point(528, 54);
+            this.btnSaveData.Name = "btnSaveData";
+            this.btnSaveData.Size = new System.Drawing.Size(100, 30);
+            this.btnSaveData.TabIndex = 6;
+            this.btnSaveData.Text = "Save";
+            this.btnSaveData.UseVisualStyleBackColor = true;
+            this.btnSaveData.Click += new System.EventHandler(this.btnSaveData_Click);
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.Location = new System.Drawing.Point(528, 90);
+            this.btnLoadData.Name = "btnLoadData";
+            this.btnLoadData.Size = new System.Drawing.Size(100, 30);
+            this.btnLoadData.TabIndex = 7;
+            this.btnLoadData.Text = "Load";
+            this.btnLoadData.UseVisualStyleBackColor = true;
+            this.btnLoadData.Click += new System.EventHandler(this.btnLoadData_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(437, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 18);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Database:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 18);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Employee ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 18);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Employee Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 18);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Status:";
+            // 
+            // MainForm
+            // 
+            this.BackColor = System.Drawing.Color.MintCream;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(653, 441);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLoadData);
+            this.Controls.Add(this.btnSaveData);
+            this.Controls.Add(this.dgvAttendance);
+            this.Controls.Add(this.btnUpdateAttendance);
+            this.Controls.Add(this.btnAddAttendance);
+            this.Controls.Add(this.cbAttendanceStatus);
+            this.Controls.Add(this.txtEmployeeName);
+            this.Controls.Add(this.txtEmployeeID);
+            this.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "MainForm";
+            this.Text = "Attendance Tracker";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAttendance)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ AttendanceTracker/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace AttendanceTracker
+{
+    public partial class MainForm : Form
+    {
+        // Employee class to hold attendance data
+        public class Employee
+        {
+            public string EmployeeID { get; set; }
+            public string EmployeeName { get; set; }
+            public string AttendanceStatus { get; set; }
+        }
+
+        private List<Employee> employeeList = new List<Employee>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // Ensure ComboBox is cleared before adding items to avoid duplicates
+            cbAttendanceStatus.Items.Clear();
+            cbAttendanceStatus.Items.AddRange(new object[] { "Present", "Absent", "On Leave" });
+            cbAttendanceStatus.SelectedIndex = 0;  // Default selection
+        }
+
+        // Save data to a CSV file
+        private void SaveDataToCSV()
+        {
+            string filePath = "employee_data.csv";  // The path where the CSV will be saved
+
+            using (StreamWriter writer = new StreamWriter(filePath, false))
+            {
+                // Write the header line (optional)
+                writer.WriteLine("EmployeeID,EmployeeName,AttendanceStatus");
+
+                // Write each employee record as a line in the CSV
+                foreach (var employee in employeeList)
+                {
+                    writer.WriteLine($"{employee.EmployeeID},{employee.EmployeeName},{employee.AttendanceStatus}");
+                }
+            }
+
+            MessageBox.Show("Data saved successfully.", "Save", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Load data from a CSV file
+        private void LoadDataFromCSV()
+        {
+            string filePath = "employee_data.csv";  // Path to your CSV file
+
+            if (File.Exists(filePath))
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line;
+
+                    // Skip the header row
+                    reader.ReadLine();
+
+                    // Clear the existing employee list before loading new data
+                    employeeList.Clear();
+
+                    // Read the data from the file line by line
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        // Split each line by commas
+                        string[] data = line.Split(',');
+
+                        // Create a new Employee object for each record and add it to the list
+                        Employee employee = new Employee
+                        {
+                            EmployeeID = data[0],
+                            EmployeeName = data[1],
+                            AttendanceStatus = data[2]
+                        };
+                        employeeList.Add(employee);
+                    }
+                }
+
+                // Update the DataGridView with the loaded data
+                LoadEmployeeData();
+
+                MessageBox.Show("Data loaded successfully.", "Load", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("The file does not exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Load employee data into the DataGridView
+        private void LoadEmployeeData()
+        {
+            dgvAttendance.Rows.Clear();  // Clear existing rows in the DataGridView
+
+            // Populate the DataGridView with the updated list of employees
+            foreach (var employee in employeeList)
+            {
+                dgvAttendance.Rows.Add(employee.EmployeeID, employee.EmployeeName, employee.AttendanceStatus);
+            }
+        }
+
+        // Add employee attendance
+        private void btnAddAttendance_Click(object sender, EventArgs e)
+        {
+            // Trim input to avoid whitespace issues
+            string employeeID = txtEmployeeID.Text.Trim();
+            string employeeName = txtEmployeeName.Text.Trim();
+            string attendanceStatus = cbAttendanceStatus.SelectedItem?.ToString();
+
+            // Validate the input fields
+            if (string.IsNullOrEmpty(employeeID) || string.IsNullOrEmpty(employeeName) || string.IsNullOrEmpty(attendanceStatus))
+            {
+                MessageBox.Show("Please fill all fields.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Validate Employee ID format (only numeric for this example)
+            if (!Regex.IsMatch(employeeID, @"^\d+$"))
+            {
+                MessageBox.Show("Please enter a valid Employee ID (numeric only).", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Create a new employee object and add it to the list
+            Employee newEmployee = new Employee
+            {
+                EmployeeID = employeeID,
+                EmployeeName = employeeName,
+                AttendanceStatus = attendanceStatus
+            };
+            employeeList.Add(newEmployee);
+
+            // Update the DataGridView to display the latest data
+            LoadEmployeeData();
+
+            // Clear input fields
+            txtEmployeeID.Clear();
+            txtEmployeeName.Clear();
+            cbAttendanceStatus.SelectedIndex = -1;
+
+            MessageBox.Show("Attendance record added successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Update employee attendance
+        private void btnUpdateAttendance_Click(object sender, EventArgs e)
+        {
+            string employeeID = txtEmployeeID.Text.Trim();
+            string attendanceStatus = cbAttendanceStatus.SelectedItem?.ToString();
+
+            if (string.IsNullOrWhiteSpace(employeeID) || string.IsNullOrWhiteSpace(attendanceStatus))
+            {
+                MessageBox.Show("Please enter a valid Employee ID and select a status.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Validate Employee ID format (only numeric for this example)
+            if (!Regex.IsMatch(employeeID, @"^\d+$"))
+            {
+                MessageBox.Show("Please enter a valid Employee ID (numeric only).", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Find the employee in the list based on EmployeeID
+            var employee = employeeList.Find(E => E.EmployeeID == employeeID);
+            if (employee == null)
+            {
+                MessageBox.Show("Employee ID not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Update the attendance status
+            employee.AttendanceStatus = attendanceStatus;
+
+            // Update the DataGridView to reflect the changes
+            LoadEmployeeData();
+
+            MessageBox.Show("Attendance record updated successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Save button click event
+        private void btnSaveData_Click(object sender, EventArgs e)
+        {
+            string filePath= @"D:\Gian\Documents\ATDB\attendance.csv";
+                using (var writer = new StreamWriter(filePath)) 
+            SaveDataToCSV();
+        }
+
+        // Load button click event
+        private void btnLoadData_Click(object sender, EventArgs e)
+        {
+            LoadDataFromCSV();
+        }
+    }
+}
